b6199358908408b5cfd7240ce029f3248ba7ad48
Justinas <jgrigas@elam.co.uk>
2019-01-22 00:18:02 +0000

    Tons of changes
    - Turning the holy hand book into a multiple pages (like an actual book)

--- js/master.js
+++ js/master.js
@@ -1,22 +1,67 @@
+const backgroundColourArray = [
+	'#d44e52', '#ec8a4b', '#ffcc68', '#80ac40', '#55a894', '#523c4e', '#2a2a3a'
+];
+
+const textColourArray = [
+	'#fff8c0', '#fff8c0', '#2a2a3a', '#2a2a3a', '#fff8c0', '#fff8c0', '#fff8c0'
+];
+
+function SetPageColour(index) {
+	let markers = document.querySelectorAll("mark");
+	let links = document.querySelectorAll("a");
+	let snippets = document.querySelectorAll("div.script");
+	ReplaceColour(markers, index);
+	ReplaceColour(links, index);
+	ReplaceColour(snippets, index);
+}
+
+function SetPageColourRandomly() {
+	let markers = document.querySelectorAll("mark");
+	let links = document.querySelectorAll("a");
+	let snippets = document.querySelectorAll("div.script");
+	ReplaceColourRandomly(markers);
+	ReplaceColourRandomly(links);
+	ReplaceColourRandomly(snippets);
+}
+
+function ReplaceColourRandomly(array) {
+	for (let i = 0; i < array.length; i++) {
+		let randomIndex = Math.floor((Math.random() * backgroundColourArray.length) + 1);
+
+		array[i].style.backgroundColor = backgroundColourArray[randomIndex];
+		array[i].style.color = textColourArray[randomIndex];
+	}
+}
+
+function ReplaceColour(array, index) {
+	for (let i = 0; i < array.length; i++) {
+		array[i].style.backgroundColor = backgroundColourArray[index];
+		array[i].style.color = textColourArray[index];
+	}
+}
+
+// This function will remove all the spaces from the scripts to allow the tabs to work properly.
+function RemoveSpacesFromCode() {
+	let preList = document.querySelectorAll("pre");
+	for (let i = 0; i < preList.length; i++) {
+		let code = preList[i].innerHTML;
+		code = code.split('  ').join('');
+		preList[i].innerHTML = code;
+	}
+}
+
 // This function will adjust the max-width of the body to keep everything pixel perfect.
 function KeepPixelPerfect() {
 	// Get the current width of the page.
-	var width = document.getElementsByTagName("html")[0].offsetWidth;
-
+	let width = document.getElementsByTagName("html")[0].offsetWidth;
 	// Get the link to the body element.
-	var body = document.getElementsByTagName("Body")[0];
-
+	let body = document.getElementsByTagName("Body")[0];
 	// Get the width of body.
-	var bodyWidth = body.offsetWidth;
-
+	let bodyWidth = body.offsetWidth;
 	// Work out the amount of space left.
-	var freeSpace = width - bodyWidth;
-
-	// The freespace is halved and forced to be even.
-	freeSpace = (freeSpace % 2 == 0) ? freeSpace : freeSpace - 1;
-	// Work out the width of the margin, in pixels.
-	var marginWidth = String(freeSpace / 2) + "px";
-
+	let freeSpace = width - bodyWidth;
+	// The free space is halved and forced to be even.
+	freeSpace = (freeSpace % 2 === 0) ? freeSpace : freeSpace - 1;
 	// Set the width of the margin.
-	body.style.marginLeft = marginWidth;
+	body.style.marginLeft = String(freeSpace / 2) + "px";
 }
